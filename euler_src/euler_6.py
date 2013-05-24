9c99daab52a719915cb684e3e1c3a3843967a846
Samuel Backlund <sbacklund@gmx.com>
2013-05-24 14:52:24 -0700

    added euler 20, 30 and revised euler 6

--- euler_src/euler_6.py
+++ euler_src/euler_6.py
@@ -1,11 +1,18 @@
-#! bin/bash/python
+#problem 6
+# for the first 100 natural numbers, find the difference between the square of the sum and the sum of the square. 
 
 
-def factorize(x):
-	factorlist = []
-	for i in range(2,x):
-		if x%i == 0:
-			factorlist.append(i)
-	return factorlist
+#numbers up to and including 100
+sum_sqrs = 0
+sum_digits = 0
+for i in range(101):
+	#add i to sum_digits
+	sum_digits += i
+	#square i and then add it to sum_sqrs
+	sum_sqrs += i**2
 
-factorize(35)
+#square the sum of the digits
+sum_squared = sum_digits**2
+
+#print the desired difference
+print(sum_squared - sum_sqrs)
